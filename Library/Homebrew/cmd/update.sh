f449bf4ca5ed50c411c574d05c87eba07136bdf4
Spencer Murray <spencermurray@users.noreply.github.com>
2020-12-15 06:25:10 -0500

    absolutely not

--- Library/Homebrew/cmd/update.sh
+++ Library/Homebrew/cmd/update.sh
@@ -393,24 +393,6 @@ EOS
   [[ -f "$HOMEBREW_LIBRARY/Taps/homebrew/homebrew-core/.git/shallow" ]] && HOMEBREW_CORE_SHALLOW=1
   [[ -f "$HOMEBREW_LIBRARY/Taps/homebrew/homebrew-cask/.git/shallow" ]] && HOMEBREW_CASK_SHALLOW=1
 
-  if [[ -n $HOMEBREW_CORE_SHALLOW || -n $HOMEBREW_CASK_SHALLOW ]]
-  then
-    odie <<EOS
-${HOMEBREW_CORE_SHALLOW:+
-  homebrew-core is a shallow clone.}${HOMEBREW_CASK_SHALLOW:+
-  homebrew-cask is a shallow clone.}
-To \`brew update\`, first run:${HOMEBREW_CORE_SHALLOW:+
-  git -C "$HOMEBREW_LIBRARY/Taps/homebrew/homebrew-core" fetch --unshallow}${HOMEBREW_CASK_SHALLOW:+
-  git -C "$HOMEBREW_LIBRARY/Taps/homebrew/homebrew-cask" fetch --unshallow}
-This restriction has been made on GitHub's request because updating shallow
-clones is an extremely expensive operation due to the tree layout and traffic of
-Homebrew/homebrew-core and Homebrew/homebrew-cask. We don't do this for you
-automatically to avoid repeatedly performing an expensive unshallow operation in
-CI systems (which should instead be fixed to not use shallow clones). Sorry for
-the inconvenience!
-EOS
-  fi
-
   export GIT_TERMINAL_PROMPT="0"
   export GIT_SSH_COMMAND="ssh -oBatchMode=yes"
 
